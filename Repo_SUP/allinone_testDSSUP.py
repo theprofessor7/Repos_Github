48a5e2348be192d296512c4e4ce50b567f0445d0
Paul-Barac <63210614+Paul-Barac@users.noreply.github.com>
2020-05-01 01:29:15 +0200

    Create allinone_testDSSUP.py

---
+++ Repo_SUP/allinone_testDSSUP.py
@@ -0,0 +1,46 @@
+import time,IPython.core.display
+
+def exercices(*args,exercice):
+     try:
+          start_test = time.time()
+          for name, value in args: globals()[name]=value
+          print("========================== début du test =========================="+'\n')
+
+          #-----------------------------------------condition(s) que l'on teste
+          #--------------------------------------------------------------------
+          if exercice==1:
+               assert moyenne([2,3,4,19,6,15,8,7])==5, "La moyenne des nombres pairs n'est pas correcte "
+          elif exercice==2:
+               assert test([2, 15, 8, 9, 4])== [9], "La liste retournée par la fonction test() n'est pas exacte"
+
+          #--------------------------------------------------------------------
+          #--------------------------------------------------------------------
+
+          stop_test = time.time()
+          tps = stop_test -start_test
+          print ("Test réussi en ",round(tps,2)," secondes",'\n')
+          print("=========================== fin du test ===========================")
+
+     except (AssertionError, NameError) as err:
+          stop_test = time.time()
+          tps = stop_test -start_test
+          print(err,"\n")
+          print ("Test échoué en ",round(tps,2)," secondes",'\n')
+          print("=========================== fin du test ===========================")
+
+# Commandes Javascript pour bloquer les accès Ctrl+C/Ctrl+V aux étudiants
+ 
+jscode_cmd = """
+document.body.addEventListener('keydown', event => {
+	
+  if (event.ctrlKey && 'cvxspwuaz'.indexOf(event.key) !== -1) {
+      event.preventDefault()}})
+	  
+jQuery(document).bind("contextmenu", function(e) {
+    e.preventDefault();});
+	
+alert("Vous pouvez commencer l'examen")
+
+"""
+display(IPython.core.display.Javascript(jscode_cmd))
+
