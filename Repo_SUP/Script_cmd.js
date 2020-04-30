db4a5c7c40a725675d8d1e8c478413668bdb13b7
Paul-Barac <63210614+Paul-Barac@users.noreply.github.com>
2020-05-01 01:51:47 +0200

    allinone

--- Repo_SUP/Script_cmd.js
+++
@@ -1,9 +0,0 @@
-document.body.addEventListener('keydown', event => {
-	
-  if (event.ctrlKey && 'cvxspwuaz'.indexOf(event.key) !== -1) {
-      event.preventDefault()}})
-	  
-jQuery(document).bind("contextmenu", function(e) {
-    e.preventDefault();});
-	
-alert("Vous pouvez commencer l'examen")
